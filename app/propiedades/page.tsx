17cee9b75eb52602d2ae344a78398552ccb7ed01
assmannmar <assmannmart@gmail.com>
2025-12-08 16:54:59 -0300

    tsx

--- app/propiedades/page.tsx
+++ app/propiedades/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 export const dynamic = "force-dynamic";
 
-
-import PropertiesContainer from '@/app/components/PropertiesContainer';
+import PropertiesContainer from "@/app/components/PropertiesContainer";
 
 export default function PropertiesPage() {
   return <PropertiesContainer />;
