131c42365dddee523c8ee7a33dd0eeb64edc1e8b
assmannmar <assmannmart@gmail.com>
2025-12-11 12:52:01 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <main className="min-h-screen bg-gray-50 flex flex-col">
 
       {/* HEADER SOBRE EL CARRUSEL */}
-      <header className="w-full py-6 absolute top-0 left-0 z-20 flex justify-center">
+      <header className="w-full py-6 absolute top-10 left-0 z-20 flex justify-center">
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
           <a href="/propiedades" className="px-6 py-3 bg-white shadow-md rounded-lg border hover:shadow-lg transition text-center text-lg font-semibold">
             Comprar
