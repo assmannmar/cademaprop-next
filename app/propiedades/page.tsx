3016f3161d364dbbe1a951caf0cdd7ba103676b6
assmannmar <assmannmart@gmail.com>
2025-12-19 22:43:16 -0300

    Update page.tsx

--- app/propiedades/page.tsx
+++ app/propiedades/page.tsx
@@ -4,5 +4,8 @@ export const dynamic = "force-dynamic";
 import PropertiesContainer from "@/app/components/PropertiesContainer";
 
 export default function PropertiesPage() {
-  return <PropertiesContainer />;
+  return (
+    <main className="pt-20 min-h-screen bg-gray-50">
+      <PropertiesContainer />;
+    </main>)
 }
