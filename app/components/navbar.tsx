39a3304777b621776f942d689ffbd8bfd573378d
assmannmar <assmannmart@gmail.com>
2026-02-25 12:37:05 -0300

    Update navbar.tsx

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -88,8 +88,8 @@ export default function Navbar() {
       href: "/emprendimientos",
       submenu:
       [
-        { name: "Residencial", href: "/emprendimientos"},
-        { name: "Industrial", href: "/emprendimientos"},
+        { name: "Residencial", href: "?div=residencial"},
+        { name: "Industrial", href: "?div=industrial"},
         { name: "Simulador", href: "/simulador"},
       ]
     },
