145a30c654465bdfeae6595b68bbbc3fe6e04df1
assmannmar <assmannmart@gmail.com>
2026-02-25 11:46:31 -0300

    Update navbar.tsx

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -11,11 +11,11 @@ const WhatsAppIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/></svg>
 );
 
-const ChevronDownIcon = () => (
-  <svg className="w-4 h-4 ml-1 inline-block transition-transform duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-  </svg>
-);
+// const ChevronDownIcon = () => (
+//   <svg className="w-4 h-4 ml-1 inline-block transition-transform duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+//     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+//   </svg>
+// );
 
 export default function Navbar() {
   const [visible, setVisible] = useState(true);
@@ -80,13 +80,22 @@ export default function Navbar() {
       [
         { name: "Casas", href: "/propiedades?tipo=house" },
         { name: "Departamentos", href: "/propiedades?tipo=apartment" },
+        { name: "Simulador", href: "/simulador" },
       ] 
     },
-    { name: "Emprendimientos", href: "/emprendimientos" },
+    { 
+      name: "Emprendimientos", 
+      href: "/emprendimientos",
+      submenu:
+      [
+        { name: "Residencial", href: "/emprendimientos"},
+        { name: "Industrial", href: "/emprendimientos"},
+        { name: "Simulador", href: "/simulador"},
+      ]
+    },
     { name: "Industria", href: "https://cademaprop.com.ar/parque-industrial/centro-logistico-consultor-inmobiliario-empresas-venta-alquiler-fracciones-galpones/" },
     { name: "Contacto", href: "/contacto" },
     { name: "Blog", href: "/blog" },
-    { name: "Simulador", href: "/simulador" },
     { name: "SINOR", href: "https://sinor.com.ar/" },
   ];
 
