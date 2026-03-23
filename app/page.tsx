1d0c4ae1ecfa03851a101abc07cfb25463ddb6bf
nahueljaguark89 <nahuel.poncedeleon@outlook.com>
2026-03-23 03:04:43 -0300

    titulo portada

--- app/page.tsx
+++ app/page.tsx
@@ -124,7 +124,7 @@ export default function HomePage() {
       </section>
 
       <section className="py-16 text-center mx-auto bg-white">
-          <h1 className='titulo-portada'>Más de 60 años acompañándote en cada etapa</h1>
+          <h2 className='titulo-portada'>Más de 60 años acompañándote en cada etapa</h2>
       </section>
 
       {/* EMPRENDIMIENTOS - CAROUSEL */}
