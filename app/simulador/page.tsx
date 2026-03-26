065260230a7a91a1d3e6031d1665e49980036c92
assmannmar <assmannmart@gmail.com>
2026-03-26 15:57:47 -0300

    Update page.tsx

--- app/simulador/page.tsx
+++ app/simulador/page.tsx
@@ -77,7 +77,7 @@ export default function SimuladorPage() {
   return (
     <>
       {loading && <FullScreenLoader />}
-      <main className="min-h-screen px-6 py-16 mt-50">
+      <main className="min-h-screen bg-gray-50 pt-[120px] pb-12">
         <section className="max-w-xl mx-auto">
           <h1 className="text-3xl font-bold mb-6">Simulador de inversión</h1>
 
