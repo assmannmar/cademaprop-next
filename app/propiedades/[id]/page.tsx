cd950a9512b548d1b2cacdef37bd2d15a11b487f
assmannmar <assmannmart@gmail.com>
2026-03-26 16:21:36 -0300

    Update page.tsx

--- app/propiedades/[id]/page.tsx
+++ app/propiedades/[id]/page.tsx
@@ -83,7 +83,7 @@ export default function PropertyDetailPage() {
       window.scrollTo({
         top: 0,
         left: 0,
-        behavior: 'instant', // 'smooth' si quieres que se vea el deslizamiento
+        behavior: 'smooth'
       });
     }, 10);
 
