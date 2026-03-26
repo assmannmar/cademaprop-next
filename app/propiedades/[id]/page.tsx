a417f4816a94c785b4aab3518e571a25768dc1b0
assmannmar <assmannmart@gmail.com>
2026-03-26 15:56:16 -0300

    Update page.tsx

--- app/propiedades/[id]/page.tsx
+++ app/propiedades/[id]/page.tsx
@@ -261,7 +261,7 @@ export default function PropertyDetailPage() {
   return (
     <>
     {loading && <FullScreenLoader />}
-    <div className="min-h-screen bg-gray-50 -mt-[70px] pt-[120px]">
+    <div className="min-h-screen bg-gray-50 pt-[120px] pb-12">
       <div className="container mx-auto px-4 py-8">
         
         {/* Breadcrumb */}
