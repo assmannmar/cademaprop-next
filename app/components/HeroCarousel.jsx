6a9cf479b23cadc8c03a075b144366115987946e
nahueljaguark89 <nahuel.poncedeleon@outlook.com>
2026-04-06 02:57:58 -0300

    hero carrusel fotos

--- app/components/HeroCarousel.jsx
+++ app/components/HeroCarousel.jsx
@@ -4,9 +4,10 @@ import { useState, useEffect } from "react";
 
 export default function HeroCarousel() {
   const images = [
-    "/carousel/1.jpg",
-    "/carousel/2.jpg",
-    "/carousel/3.jpg"
+    "/carousel/3.jpg",
+    "/carousel/4.jpg",
+    "/carousel/5.jpg",
+    "/carousel/6.jpg"
   ];
 
   const [index, setIndex] = useState(0);
