df408b85001dd9b40d1cb31f636d34bed697cd45
assmannmar <assmannmart@gmail.com>
2026-05-21 14:51:18 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: '/propiedad-:id-:slug*',
-        destination: '/propiedades/:id-casa-venta-:slug*',
-        permanent: true, // 301 (permanente)
+        source: '/propiedad-:id([0-9]+)-:rest*',
+        destination: '/propiedades/:id-casa-venta-:rest*',
+        permanent: true,
       }
     ]
   }
